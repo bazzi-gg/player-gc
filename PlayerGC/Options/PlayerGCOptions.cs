e5ee17ca1292583ff3db81c852f6a56a2b04ae45
Plorence <ploseok@gmail.com>
2022-07-19 21:22:06 +0900

    create option class

---
+++ PlayerGC/Options/PlayerGCOptions.cs
@@ -0,0 +1,10 @@
+namespace PlayerGC.Options;
+
+public class PlayerGCOptions
+{
+    public const string ConfigurationKey = "PlayerGC";
+    /// <summary>
+    /// ms
+    /// </summary>
+    public int LoopDelay { get; set; }
+}
